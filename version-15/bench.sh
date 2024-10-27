be662385d085becf6202d104ac124c51828b960e
vr-greycube <vijay@greycube.in>
2024-10-27 13:35:31 +0530

    feat: add version-14

--- version-15/bench.sh
+++ version-15/bench.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Check if an argument is provided
-# if [ "$#" -ne 1 ]; then
-#     echo "Usage: $0 <argument>"
-#     exit 1
-# fi
-
-# Executing the bench command within the 'watch' service container
-docker compose exec -T --user frappe watch /bin/bash -c "/home/frappe/.local/bin/bench $@"
+docker compose exec -w /home/frappe/frappe-bench --user frappe watch sh
